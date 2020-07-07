5dcb6f445d3b875f2d4d0f24570b1325b9608551
mm2m123 <eminem13@126.com>
2020-07-07 17:16:51 +0800

    001 - Lecture 1 - Course Logistics and Introduction to SwiftUI

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by 张毅成 on 2020/7/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+            .foregroundColor(.orange)
+            .padding(.all, 10)
+            .font(.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 25, style: .continuous).stroke(lineWidth: 3.0)
+                RoundedRectangle(cornerRadius: 25, style: .continuous).fill().foregroundColor(.white)
+                Text("👻")
+            }else{
+                RoundedRectangle(cornerRadius: 25, style: .continuous).fill()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 张毅成 on 2020/7/7.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
